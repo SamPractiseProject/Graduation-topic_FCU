8db70115acab806c8958617641fe077fb86d457f
楊寧 <yyaoning@gmail.com>
2021-04-14 14:19:09 +0800

    控制速度

--- 控制輪子/基本開馬達.py
+++ 控制輪子/基本開馬達.py
@@ -5,15 +5,25 @@ from curses import wrapper
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
+RIN1 = 7
+RIN2 = 11
+RIN3 = 25
+RIN4 = 10
+LIN1 = 17
+LIN2 = 18
+LIN3 = 22
+LIN4 = 23
 
-GPIO.setup(10, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(7,  GPIO.OUT)
+
+
+GPIO.setup(RIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN4, GPIO.OUT, initial=GPIO.LOW)
 
 stdscr = curses.initscr()
 stdscr.clear()
@@ -23,56 +33,56 @@ while True:
 # Quit
     if ch == 'q':
        curses.endwin()
-       GPIO.output(17, False)
-       GPIO.output(18, False)
-       GPIO.output(22, False)
-       GPIO.output(23, False)
-       GPIO.output(10, False)
-       GPIO.output(25, False)
-       GPIO.output(11, False)
-       GPIO.output(7, False)
+       GPIO.output(LIN1, False)     #GPIO17
+       GPIO.output(LIN2, False)     #GPIO18
+       GPIO.output(LIN3, False)     #GPIO22
+       GPIO.output(LIN4, False)     #GPIO23
+       GPIO.output(RIN1, False)     #GPIO7
+       GPIO.output(RIN2, False)     #GPIO11
+       GPIO.output(RIN3, False)     #GPIO25
+       GPIO.output(RIN4, False)     #GPIO10
        break
 
 # Forward
     if ch == 'w':
-       GPIO.output(17, True)
-       GPIO.output(18, False)
-       GPIO.output(22, False)
-       GPIO.output(23, True)
-       GPIO.output(10, True)
-       GPIO.output(25, False)
-       GPIO.output(11, False)
-       GPIO.output(7, True)
+       GPIO.output(LIN1, True)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, True)
 
 # Backward
     if ch == 'x':
-       GPIO.output(17, False)
-       GPIO.output(18, True)
-       GPIO.output(22, True)
-       GPIO.output(23, False)
-       GPIO.output(10, False)
-       GPIO.output(25, True)
-       GPIO.output(11, True)
-       GPIO.output(7, False)
+       GPIO.output(LIN1, False)
+       GPIO.output(LIN2, True)
+       GPIO.output(LIN3, True)
+       GPIO.output(LIN4, False)
+       GPIO.output(RIN1, False)
+       GPIO.output(RIN2, True)
+       GPIO.output(RIN3, True)
+       GPIO.output(RIN4, False)
 
 # Turn Right
     if ch == 'd':
-       GPIO.output(17, True)
-       GPIO.output(18, False)
-       GPIO.output(22, False)
-       GPIO.output(23, True)
-       GPIO.output(10, False)
-       GPIO.output(25, False)
-       GPIO.output(11, False)
-       GPIO.output(7, True)
+       GPIO.output(LIN1, True)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, False)
 
 # Turn Left
     if ch == 'a':
-       GPIO.output(17, False)
-       GPIO.output(18, False)
-       GPIO.output(22, False)
-       GPIO.output(23, True)
-       GPIO.output(10, True)
-       GPIO.output(25, False)
-       GPIO.output(11, False)
-       GPIO.output(7, True)
+       GPIO.output(LIN1, False)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, True)
---
+++ 控制輪子/控制速度.py
@@ -0,0 +1,152 @@
+import RPi.GPIO as GPIO
+import curses
+import time
+from curses import wrapper
+from espeak import espeak
+GPIO.setmode(GPIO.BCM)
+
+RIN1 = 7
+RIN2 = 11
+RIN3 = 25
+RIN4 = 10
+LIN1 = 17
+LIN2 = 18
+LIN3 = 22
+LIN4 = 23
+
+ENR_A = 6
+ENR_B = 12
+ENL_A = 19
+ENL_B = 16
+
+GPIO.setup(RIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN4, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(ENR_A, GPIO.OUT)
+GPIO.setup(ENR_B, GPIO.OUT)
+GPIO.setup(ENL_A, GPIO.OUT)
+GPIO.setup(ENL_B, GPIO.OUT)
+
+stdscr = curses.initscr()
+stdscr.clear()
+pwm_R_A = GPIO.PWM(ENR_A,500)  #ENR_A设置为PWM控制
+pwm_R_B = GPIO.PWM(ENR_B,500)  #ENR_B设置为PWM控制
+pwm_L_A = GPIO.PWM(ENL_A,500)  #ENL_A设置为PWM控制
+pwm_L_B = GPIO.PWM(ENL_B,500)  #NL_B设置为PWM控制
+
+pwm_R_A.start(0)
+pwm_R_B.start(0)
+pwm_L_A.start(0)
+pwm_L_B.start(0)
+
+
+def Speed_Control(x):
+    value=0
+    global pwm_L_A, pwm_L_B, pwm_R_A, pwm_R_B
+    if x == 'q':
+        pwm_R_A.ChangeDutyCycle(0)
+        pwm_L_A.ChangeDutyCycle(0)
+        pwm_R_B.ChangeDutyCycle(0)
+        pwm_L_B.ChangeDutyCycle(0)
+        for i in range(100,0,-10):
+            pwm_R_A.ChangeDutyCycle(i)
+            pwm_L_A.ChangeDutyCycle(i)
+            pwm_R_B.ChangeDutyCycle(i)
+            pwm_L_B.ChangeDutyCycle(i)
+            time.sleep(1)
+
+    if x == 'w':
+        pwm_R_A.ChangeDutyCycle(0)
+        pwm_L_A.ChangeDutyCycle(0)
+        pwm_R_B.ChangeDutyCycle(0)
+        pwm_L_B.ChangeDutyCycle(0)
+        for i in range(0,100,10):
+            pwm_R_A.ChangeDutyCycle(i)
+            pwm_L_A.ChangeDutyCycle(i)
+            pwm_R_B.ChangeDutyCycle(i)
+            pwm_L_B.ChangeDutyCycle(i)
+            time.sleep(1)
+    if x == 'x':
+        pwm_R_A.ChangeDutyCycle(0)
+        pwm_L_A.ChangeDutyCycle(0)
+        pwm_R_B.ChangeDutyCycle(0)
+        pwm_L_B.ChangeDutyCycle(0)
+        for i in range(0,100,10):
+            pwm_R_A.ChangeDutyCycle(i)
+            pwm_L_A.ChangeDutyCycle(i)
+            pwm_R_B.ChangeDutyCycle(i)
+            pwm_L_B.ChangeDutyCycle(i)
+            time.sleep(1)
+
+
+
+
+while True:
+    ch = stdscr.getkey()
+# Quit
+    if ch == 'q':
+       curses.endwin()
+       GPIO.output(LIN1, False)     #GPIO17
+       GPIO.output(LIN2, False)     #GPIO18
+       GPIO.output(LIN3, False)     #GPIO22
+       GPIO.output(LIN4, False)     #GPIO23
+       GPIO.output(RIN1, False)     #GPIO7
+       GPIO.output(RIN2, False)     #GPIO11
+       GPIO.output(RIN3, False)     #GPIO25
+       GPIO.output(RIN4, False)     #GPIO10
+       Speed_Control(ch)
+       break
+
+# Forward
+    if ch == 'w':
+       GPIO.output(LIN1, True)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, True)
+       Speed_Control(ch)
+
+# Backward
+    if ch == 'x':
+       GPIO.output(LIN1, False)
+       GPIO.output(LIN2, True)
+       GPIO.output(LIN3, True)
+       GPIO.output(LIN4, False)
+       GPIO.output(RIN1, False)
+       GPIO.output(RIN2, True)
+       GPIO.output(RIN3, True)
+       GPIO.output(RIN4, False)
+       Speed_Control(ch)
+
+# Turn Right
+    if ch == 'd':
+       GPIO.output(LIN1, True)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, False)
+       Speed_Control(ch)
+
+# Turn Left
+    if ch == 'a':
+       GPIO.output(LIN1, False)
+       GPIO.output(LIN2, False)
+       GPIO.output(LIN3, False)
+       GPIO.output(LIN4, True)
+       GPIO.output(RIN1, True)
+       GPIO.output(RIN2, False)
+       GPIO.output(RIN3, False)
+       GPIO.output(RIN4, True)
+       Speed_Control(ch)
